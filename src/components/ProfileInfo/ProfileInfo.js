74beaa24c01bef58876a92ab9998ff4b2f36ec0f
Valerris <ya@vlrrs>
2020-06-06 17:32:50 +0300

    removed form and unnecessery img check

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -5,22 +5,14 @@ import ProfileForm from "../../containers/forms/Profile/Profile";
 import classes from "./ProfileInfo.module.css";
 
 const ProfileInfo = ({ profileInfo, postEdit }) => {
-	const { loadingInfo, loadingEdit } = useSelector((state) => state.profile);
+	const { loadingInfo, loadingEdit } = useSelector(
+		(state) => state.profile
+	);
 
 	const UI = profileInfo ? (
 		<div className={classes["profileInfo-wrapper"]}>
-			{/* {profileInfo.img ? (
-				<img
-					src={profileInfo.profile.img}
-					alt={`pic-${profileInfo.user.name}`}
-				/>
-			) : (
-				
-			)} */}
 			<div className={classes["profileInfo"]}>
 				<div className={classes.profileInfo__info}>
-					{!profileInfo.profile.imageUrl ? <p>Upload your image.</p> : null}
-
 					<div className={classes.profileInfo__pic}>
 						{!profileInfo.profile.imageUrl ? null : (
 							<img
@@ -41,9 +33,9 @@ const ProfileInfo = ({ profileInfo, postEdit }) => {
 						{profileInfo.profile.email}
 					</div>
 				</div>
-				<div className={classes.profileInfo__edit}>
+				{/* <div className={classes.profileInfo__edit}>
 					<ProfileForm loadingEdit={loadingEdit} postEdit={postEdit} />
-				</div>
+				</div> */}
 			</div>
 		</div>
 	) : (
